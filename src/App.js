138e4577b753c26c9d6dfc06cd69c6ab73630876
Maria <marysiahalamska@gmail,com>
2026-04-21 16:46:25 +0200

    added components that do nothing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import Lottie from "lottie-react";
 import animationData1 from "./Lilia_Light.json";
 import animationData2 from "./DarkLight.json";
+import Card from "./card.js";
+import Button from "./Button.js";
 
 function App() {
   return (
-    <body>
-      <Lottie className="lottie" animationData={animationData1} />
-      <p>wawaw</p>
-      <Lottie className="lottie" animationData={animationData2} />
-    </body>
+    <>
+      <body>
+        <Lottie className="lottie" animationData={animationData1} />
+        <p>here's current year! {new Date().getFullYear()}</p>
+        <Lottie className="lottie" animationData={animationData2} />
+        <Card></Card>
+        <Button></Button>
+      </body>
+    </>
   );
 }
 console.log("test");
---
+++ src/card.js
@@ -0,0 +1,13 @@
+function Card() {
+  return (
+    <div className="card">
+      <img></img>
+      <h2>Card title</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+        voluptate.
+      </p>
+    </div>
+  );
+}
+export default Card;
